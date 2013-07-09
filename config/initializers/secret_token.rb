1004dc7cd9a49d9d460d46dbdc06bdbe10be1d3e
elizahyde <elizabeth@elizahyde.com>
2013-07-09 15:51:02 -0700

    first commit. Gemfile setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Luckypots::Application.config.secret_token = '9ebf9cc8e19facf25ec951651f9b0856a128032cbeb13e187d998ce394b6e0a1df30f20b1352f8e1352c36611e632a9795e2d893f4de68f01693c6b5c87595bd'
